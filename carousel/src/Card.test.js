868004d0bf6f2fad576f31b4fd6478c8a21a75b5
brandon mcintosh <brandon.l.mcintosh@gmail.com>
2021-07-05 18:39:24 -0700

    fixing tests

---
+++ carousel/src/Card.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Card from './Card';
+
+test("should render", () => {
+    render(<Card />);
+});
+
+test('it matches snapshot', () => {
+    const {asFragment} = render(<Card />);
+    expect(asFragment()).toMatchSnapshot();
+});
+
+
+
--- carousel/src/Carousel.test.js
+++ carousel/src/Carousel.test.js
@@ -2,6 +2,15 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 
+it('should render', () => {
+  render(<Carousel />);
+});
+
+test('it matches snapshot', () => {
+  const {asFragment} = render(<Carousel />);
+  expect(asFragment()).toMatchSnapshot();
+})
+
 it("works when you click on the right arrow", function() {
   const { queryByTestId, queryByAltText } = render(<Carousel />);
 
